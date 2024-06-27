182546e5ee3a51f7c34dbff99eb0277b087a86ff
garold664 <garold664@gmail.com>
2024-06-27 20:45:28 +0300

    creates Button ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Button from './components/ui/Button';
+
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <div className="text-3xl text-blue-500">
+      Home
+      <Button size={'sm'} variant={'ghost'}>
+        Hello
+      </Button>
+    </div>
+  );
 }
