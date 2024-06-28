b7d28ebd2d06585cb5791154ca89b408e53a3791
garold664 <garold664@gmail.com>
2024-06-28 11:30:02 +0300

    sets up login page

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,12 +4,16 @@ import type { JWT } from 'next-auth/jwt';
 type UserId = string;
 
 declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT {
     id: UserId;
   }
 }
 
 declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
   interface Session {
     user: User & {
       id: UserId;
