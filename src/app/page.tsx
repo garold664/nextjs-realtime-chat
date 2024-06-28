7894c29a41780ec048bb51eeb55774b74e8f3d51
garold664 <garold664@gmail.com>
2024-06-28 08:48:44 +0300

    fixed folders structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Button from './components/ui/Button';
-import { db } from './components/ui/lib/db';
+import Button from '../components/ui/Button';
+import { db } from '../lib/db';
 
 export default async function Home() {
-  await db.set('hello', 'world');
+  await db.set('hello', 'world!!!');
   return <div className="text-3xl text-blue-500">Home</div>;
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import { VariantProps, cva } from 'class-variance-authority';
 import { Loader2 } from 'lucide-react';
 import { ButtonHTMLAttributes } from 'react';
-import { cn } from './lib/util';
+import { cn } from '../../lib/util';
 
 const buttonVariants = cva(
   'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
