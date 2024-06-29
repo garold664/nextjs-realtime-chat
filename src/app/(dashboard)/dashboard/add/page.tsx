436c46ff66fa4f05b66af05bb3ac860e262ea0ed
garold664 <garold664@gmail.com>
2024-06-29 09:06:47 +0300

    creates form with validation for sending friend request

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,11 @@
+import AddFriendButton from '@/components/ui/AddFriendButton';
+import React from 'react';
+
+export default function page() {
+  return (
+    <main className="font-bold text-5xl mb-8">
+      <h2>Add a friend</h2>
+      <AddFriendButton />
+    </main>
+  );
+}
