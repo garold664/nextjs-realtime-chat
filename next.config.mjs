1c09a32528c11d727d4e6b0329c176de85f5a83f
garold664 <garold664@gmail.com>
2024-07-06 18:48:00 +0300

    fixes next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
