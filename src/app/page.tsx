1c8b8d513013bb3d3a1a53659fa04be14c525285
garold664 <garold664@gmail.com>
2024-07-06 09:41:54 +0300

    adds login link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Button from '../components/ui/Button';
 import { db } from '../lib/db';
 
@@ -5,6 +6,7 @@ export default async function Home() {
   await db.set('hello', 'world!!!');
   return (
     <div className="text-3xl text-blue-500">
+      <Link href={'/login'}>Sign in</Link>
       <Button variant={'ghost'}>Home</Button>
     </div>
   );
