9f208e8111683b1e83e88ac68aa8c7787ef1181c
garold664 <garold664@gmail.com>
2024-07-06 17:15:05 +0300

    deletes an old singOut button and makes dashboard/add page server component again

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,22 +1,16 @@
-'use client';
 import Button from '@/components/ui/Button';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
-import { signOut } from 'next-auth/react';
 
 const Page = () => {
   // const session = await getServerSession(authOptions);
 
-  function handleSignOut() {
-    signOut();
-  }
   // return <pre>{JSON.stringify(session)}</pre>;
   return (
     <div>
       <h1 className="text-5xl font-bold text-center p-5 underline underline-offset-8">
         Dashboard
       </h1>
-      <Button onClick={handleSignOut}>Sign out</Button>
     </div>
   );
 };
