9e59f02c60e3c0b2b3b89cf3fe0f5cf335d1a34a
garold664 <garold664@gmail.com>
2024-07-08 13:49:49 +0300

    finishes chat page functionallity

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,3 +1,10 @@
+import { fetchRedis } from '@/helpers/redis';
+import { authOptions } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { messageArrayValidator } from '@/lib/validations/message';
+import { Message, User } from '@/types/db';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 interface ChatPageProps {
@@ -6,7 +13,43 @@ interface ChatPageProps {
   };
 }
 
-export default function ChatPage({ params }: ChatPageProps) {
+async function getChatMessages(chatId: string) {
+  try {
+    const results: string[] = await fetchRedis(
+      'zrange',
+      `chat:${chatId}:messages`,
+      0,
+      -1
+    );
+
+    const dbMessages = results.map((message) => JSON.parse(message) as Message);
+    const reversedDbMessages = dbMessages.toReversed();
+    const messages = messageArrayValidator.parse(reversedDbMessages);
+
+    return messages;
+  } catch (error) {
+    notFound();
+  }
+}
+
+export default async function ChatPage({ params }: ChatPageProps) {
   const { chatId } = params;
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) notFound();
+
+  const { user } = session;
+
+  const [userId1, userId2] = chatId.split('--');
+
+  if (userId1 !== user.id && userId2 !== user.id) notFound();
+
+  const chatPartnerId = userId1 !== user.id ? userId1 : userId2;
+
+  const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
+
+  const initMessages = await getChatMessages(chatId);
+
   return <div>{chatId}</div>;
 }
