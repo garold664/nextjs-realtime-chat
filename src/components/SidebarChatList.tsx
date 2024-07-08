b2122a0a6647962cccd68211f37873567f4d9cdf
garold664 <garold664@gmail.com>
2024-07-08 18:00:47 +0300

    adds more styling to SidebarChatList

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -40,9 +40,14 @@ export default function SidebarChatList({
                 sessionId,
                 friend.id
               )}`}
-              className="text-xs font-semibold leading-6 text-gray-400"
+              className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
             >
               {friend.name}
+              {unseenMessagesCount > 0 && (
+                <div className="bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center">
+                  {unseenMessagesCount}
+                </div>
+              )}
             </a>
           </li>
         );
