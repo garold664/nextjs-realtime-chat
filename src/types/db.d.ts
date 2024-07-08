8a59f5436c1a9715d70319f37694e2a10ea74634
garold664 <garold664@gmail.com>
2024-07-08 12:38:42 +0300

    creates new types for chats

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -4,3 +4,22 @@ export type User = {
   email: string;
   image: string;
 };
+
+interface Chat {
+  id: string;
+  messages: Message[];
+}
+
+interface Message {
+  id: string;
+  senderId: string;
+  receiverId: string;
+  text: string;
+  timestamp: number;
+}
+
+interface FriendRequest {
+  id: string;
+  senderId: string;
+  receiverId: string;
+}
