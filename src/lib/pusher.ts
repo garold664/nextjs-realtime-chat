f6b1b2ea01264add1cc05454ff078590d650a433
garold664 <garold664@gmail.com>
2024-07-09 17:46:40 +0300

    fixes an error with env var

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -3,12 +3,15 @@ import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_APP_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: 'eu',
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(process.env.PUSHER_APP_KEY!, {
-  cluster: 'eu',
-});
+export const pusherClient = new PusherClient(
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  {
+    cluster: 'eu',
+  }
+);
