3490e2780194ce620d5c9121a4bcc17821d445a5
garold664 <garold664@gmail.com>
2024-07-09 17:38:44 +0300

    triggers pusher on the server

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from '@/helpers/redis';
 import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
+import { pusherServer } from '@/lib/pusher';
+import { toPusherKey } from '@/lib/util';
 import { addFriendValidator } from '@/lib/validations/add-friend';
 import { getServerSession } from 'next-auth';
 import { z } from 'zod';
@@ -52,6 +54,17 @@ export async function POST(request: Request) {
     if (isAlreadyFriends) {
       return new Response('Already friends with this user', { status: 400 });
     }
+
+    //! Valid request, send friend request
+
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      'incoming_friend_request',
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
 
     db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
     return new Response('OK');
