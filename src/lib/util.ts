13690acb928ec0be29037017242aa543a3ee115f
garold664 <garold664@gmail.com>
2024-07-09 17:06:47 +0300

    creates helper function to transform keys for pusher

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,6 +6,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function toPusherKey(key: string) {
+  return key.replaceAll(':', '__');
+}
+
 export function chatHrefConstructor(id1: string, id2: string) {
   const sortedIds = [id1, id2].sort();
   return sortedIds.join('--');
