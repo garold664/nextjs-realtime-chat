90837332026e0017eddc921d1cdd5ac2c22f1c68
garold664 <garold664@gmail.com>
2024-07-11 20:46:04 +0300

    fixes a mistype

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -30,7 +30,7 @@ export default async function RequestsPage() {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
     <main className="pt-8">
-      <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+      <h1 className="font-bold text-5xl mb-8">Friend requests</h1>
       <div className="flex flex-col gap-4">
         <FriendRequests
           incomingFriendRequests={incomingFriendRequests}
